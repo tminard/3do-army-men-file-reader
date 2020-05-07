24c808e2f2d4f1077ed4df462e94298e8844bf4a
tminard <adelphospro@gmail.com>
2020-05-07 13:36:18 -0400

    Add project files.

---
+++ amm/FractionBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMMEdit.amm
+{
+    class FractionBlock
+    {
+        UInt32 numRifleSquads; // the number of initial rifle squads in the game
+        UInt32 numGrenaderSquads;
+        UInt32 numFlamerSquads;
+        UInt32 numBazookaSquads;
+        UInt32 numEngineerSquads;
+        UInt32 numMorterSquads;
+        UInt32 numSpecial1Squads; // miners, zombies, etc
+        UInt32 numSpecial2Squads; // prisoners, etc
+        UInt32 numJeepUnits;
+        UInt32 numTankUnits;
+        UInt32 numHalfTrackUnits;
+        UInt32 numUnknownVehicle1Units; // truck?
+        UInt32 unknownField1; // no impact on high value. Crash on low. Either padding, or reserved for specific maps. Perhaps for special vehicles (heli?)
+        UInt32 numTotalUnits; // total number of squads in the fraction
+    }
+}
---
+++ amm/MapFile.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMMEdit.amm
+{
+    class MapFile
+    {
+        private readonly Uri file;
+
+        private Dictionary<FractionBlock, List<UnitBlock>> armies; // ordered list of fractions and their units. Order: Green, Tan, Blue, Grey
+
+        public MapFile(Uri file)
+        {
+            this.file = file ?? throw new ArgumentNullException(nameof(file));
+        }
+
+        private void loadFile()
+        {
+            // Load the given file, seek to the end, and populate fractions block
+        }
+    }
+}
